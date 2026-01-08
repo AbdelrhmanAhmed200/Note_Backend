5724f2b9699f5561cbfb9659db5a2f4df6939a0c
AbdelrhmanAhmed200 <abdelrahmanahmedfarid988@gmail.com>
2026-01-08 19:50:22 +0200

    v.1

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./API/config/db');
-const userrouter = require('./API/features/user/routes/user_router')
+const userrouter = require('./API/features/user/routes/user_router');
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
 connectDB();
 app.get('/', (req, res) => res.send('Welcome Here!'));
 
-app.use('/note',userrouter)
-app.listen(process.env.port, () => console.log('server running'));
+app.use('/note', userrouter);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
