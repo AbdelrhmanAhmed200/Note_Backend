80bf03af1af38ec2093cb6674564f6bd38d35fdc
AbdelrhmanAhmed200 <abdelrahmanahmedfarid988@gmail.com>
2026-01-28 16:51:42 +0200

    v.1

--- API/features/user/Middleware/auth.js
+++ API/features/user/Middleware/auth.js
@@ -1,4 +1,3 @@
-
 const { verifyJwt } = require('../../../config/jwt_token');
 
 module.exports = (req, res, next) => {
@@ -8,7 +7,8 @@ module.exports = (req, res, next) => {
       return res.status(401).json({ message: 'No token provided' });
     }
 
-    const token = authHeader;
+    const token = authHeader;  // Get the token after "Bearer "
+    
     const decoded = verifyJwt(token);
 
     req.user = decoded;
